b2b4e286d6f5d34f4ab959a92b417eb17ffb8076
Kenneth Reitz <me@kennethreitz.org>
2017-08-28 21:42:21 -0400

    Update __version__.py

--- mypackage/__version__.py
+++ mypackage/__version__.py
@@ -1,6 +1,6 @@
-#     ___     ( )  ___      ___       __
-#   //   ) ) / / //   ) ) //___) ) //   ) ) ||  / /
-#  //___/ / / / //___/ / //       //   / /  || / /
-# //       / / //       ((____   //   / /   ||/ /
+# 8b    d8 Yb  dP 88""Yb    db     dP""b8 88  dP    db     dP""b8 888888
+# 88b  d88  YbdP  88__dP   dPYb   dP   `" 88odP    dPYb   dP   `" 88__
+# 88YbdP88   8P   88"""   dP__Yb  Yb      88"Yb   dP__Yb  Yb  "88 88""
+# 88 YY 88  dP    88     dP""""Yb  YboodP 88  Yb dP""""Yb  YboodP 888888
 
 __version__ = '5.2.0'
