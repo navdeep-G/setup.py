59cfa99b99d87bf2cb2e9176c6dfcacafb532023
Kenneth Reitz <me@kennethreitz.org>
2018-05-04 09:33:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ REQUIRED = [
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-# Note: this will only work if 'README.rst' is present in your MANIFEST.in file!
-with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 # Load the package's __version__.py module as a dictionary.
@@ -89,6 +89,7 @@ setup(
     version=about['__version__'],
     description=DESCRIPTION,
     long_description=long_description,
+    long_description_content_type='text/markdown'
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
