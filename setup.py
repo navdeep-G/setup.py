a0f9ab13c61d3712455266b26051f9cf74193c75
Dev Aggarwal <devxpy@gmail.com>
2018-05-17 19:22:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ REQUIRED = [
 
 # What packages are optional?
 EXTRAS = {
-    # 'my fancy feature': ['django'],
+    # 'fancy feature': ['django'],
 }
 
 # The rest you shouldn't have to touch too much :)
