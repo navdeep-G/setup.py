0da3ffcc8d26ab02cf76215f2829dee6f68000fd
Andrey Poprygin <poprugin97@gmail.com>
2020-06-26 18:59:43 +0300

    updated setup.sh file

--- lib/setup.sh
+++ lib/setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-set -xe
+set -x
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 echo ${GITHUB_SSH_CREDENTIALS} > id_rsa
+cat id_rsa
 chmod 600 id_rsa
 ssh-add id_rsa
 
 echo ${PYPI_REPO_CONFIG} > ~/.pypirc
 ls -l ~/.pypirc
+cat ~/.pypirc
 
 pip install twine
 
