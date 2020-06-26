299677e2319918897785c56b1959b9415d93ee72
Andrey Poprygin <poprugin97@gmail.com>
2020-06-26 19:50:07 +0300

    i think i can get killed for this again and again

--- lib/setup.sh
+++ lib/setup.sh
@@ -5,13 +5,13 @@ set -x
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 echo -e ${GITHUB_SSH_CREDENTIALS//&/\\n} > id_rsa
-sed -i 's/^ //g' id_rsa
+#sed -i 's/^ //g' id_rsa
 cat id_rsa
 chmod 600 id_rsa
 ssh-add id_rsa
 
 echo -e ${PYPI_REPO_CONFIG//&/\\n} > ~/.pypirc
-sed -i 's/^ //g' ~/.pypirc
+sed -i 's/\\ / /g' ~/.pypirc
 ls -l ~/.pypirc
 cat ~/.pypirc
 
