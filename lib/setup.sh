6f81484f8659c2789edb6fb068ea62d24dad3563
Andrey Poprygin <poprugin97@gmail.com>
2020-06-26 20:40:29 +0300

    changed cred file location

--- lib/setup.sh
+++ lib/setup.sh
@@ -4,12 +4,12 @@ set -x
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-echo -e ${GITHUB_SSH_CREDENTIALS} > id_rsa_base64
-sed -i 's/^ //g' id_rsa_base64
-base64 -d id_rsa_base64 > id_rsa
-cat id_rsa
-chmod 600 id_rsa
-ssh-add id_rsa
+echo -e ${GITHUB_SSH_CREDENTIALS} > ~/id_rsa_base64
+sed -i 's/^ //g' ~/id_rsa_base64
+base64 -d id_rsa_base64 > ~/id_rsa
+cat ~/id_rsa
+chmod 600 ~/id_rsa
+ssh-add ~/id_rsa
 
 echo -e ${PYPI_REPO_CONFIG} > ~/.pypirc_base64
 sed -i 's/^ //g' ~/.pypirc_base64
