babfa8cb193cd77ab3d4c7a0705fca8ae709ced6
Andrey Poprygin <poprugin97@gmail.com>
2020-06-26 20:06:27 +0300

    using base64 now

--- lib/setup.sh
+++ lib/setup.sh
@@ -4,15 +4,16 @@ set -x
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-echo -e ${GITHUB_SSH_CREDENTIALS//&/\\n} > id_rsa
-#sed -i 's/^ //g' id_rsa
+echo -e ${GITHUB_SSH_CREDENTIALS} > id_rsa_base64
+sed -i 's/^ //g' id_rsa_base64
+base64 -d id_rsa_base64 > id_rsa
 cat id_rsa
 chmod 600 id_rsa
 ssh-add id_rsa
 
-echo -e ${PYPI_REPO_CONFIG//&/\\n} > ~/.pypirc
-sed -i 's/\\ / /g' ~/.pypirc
-ls -l ~/.pypirc
+echo -e ${PYPI_REPO_CONFIG} > ~/.pypirc_base64
+sed -i 's/^ //g' ~/.pypirc_base64
+base64 -d ~/.pypirc_base64 > ~/.pypirc
 cat ~/.pypirc
 
 pip install twine
