5eebb1e408d2d17938e59f3a3e82dcb6fe0fffcc
Andrey Poprygin <poprugin97@gmail.com>
2020-06-26 19:25:21 +0300

    crutch to decode env vars v4

--- lib/setup.sh
+++ lib/setup.sh
@@ -4,7 +4,7 @@ set -x
 
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
-echo -e ${GITHUB_SSH_CREDENTIALS//&/\\n} > id_rsa
+echo -e ${GITHUB_SSH_CREDENTIALS//& /\\n} > id_rsa
 cat id_rsa
 chmod 600 id_rsa
 ssh-add id_rsa
